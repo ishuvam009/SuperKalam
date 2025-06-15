851a0ae58c730712834c84e2f433b8cc0ab0de22
ishuvam009 <ishuvam549@gmail.com>
2025-06-15 23:58:34 +0530

    Commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,21 @@
 import img1 from "../assets/logo.jpg";
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
-    return(
+  return (
     <>
-        <div className='flex justify-between  '>
-          <div className='flex items-center gap-x-2 pt-4 pl-20'>
-            <img className='h-8' src={img1} alt="UPSC" />
-            <p className='text-xl'>SuperKalam</p>
-          </div>
-          <div className='flex items-center gap-x-12 pt-4 pr-20'>
-            <p className='text-[#3280F6] text-lg font-medium hover:bg-blue-50 hover:rounded-4xl hover:p-1'>Explore Batches</p>
-            <button className='text-[#3280F6] text-lg font-medium border border-[#3280F6]-1 px-3 py-2 rounded-lg hover:text-white hover:bg-[#3280F6]'>Login</button>
-          </div>
+      <div className='flex justify-between  '>
+        <div className='flex items-center gap-x-2 pt-4 pl-20'>
+          <img className='h-8' src={img1} alt="UPSC" />
+          <p className='text-xl'>SuperKalam</p>
         </div>
+        <div className='flex items-center gap-x-12 pt-4 pr-20'>
+          <p className='text-[#3280F6] text-lg font-medium hover:bg-blue-50 hover:rounded-4xl hover:p-1'>Explore Batches</p>
+          <Link to="/login">
+            <button className='text-[#3280F6] text-lg font-medium border border-[#3280F6]-1 px-3 py-2 rounded-lg hover:text-white hover:bg-[#3280F6]'>Login</button>
+          </Link>
+        </div>
+      </div>
     </>
-    )
+  )
 }
