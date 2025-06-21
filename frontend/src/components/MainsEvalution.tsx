6cd55ee3969889aabae3394c72560aac9e3cd19e
ishuvam009 <ishuvam549@gmail.com>
2025-06-22 00:02:27 +0530

    Commit

--- frontend/src/components/MainsEvalution.tsx
+++ frontend/src/components/MainsEvalution.tsx
@@ -2,9 +2,9 @@ export const MainsEvalution = () => {
     return (
         <>
             <div>
-                <div>
-                    <p>Evaluate Mains Answer</p>
-                    <button>Star Evalution</button>
+                <div className="flex justify-between p-4 mt-6">
+                    <p className="text-2xl font-semibold">Evaluate Mains Answer</p>
+                    <button className="text-white font-medium bg-[#2D80F6] rounded-md py-2 px-4 hover:brightness-95 cursor-pointer">Star Evalution</button>
                 </div>
             </div>
         </>
