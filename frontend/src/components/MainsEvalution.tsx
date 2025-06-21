a9843bea97abd040bf5e225aa995ddec24a59ad4
ishuvam009 <ishuvam549@gmail.com>
2025-06-21 23:57:28 +0530

    Commit

--- frontend/src/components/MainsEvalution.tsx
+++ frontend/src/components/MainsEvalution.tsx
@@ -2,7 +2,10 @@ export const MainsEvalution = () => {
     return (
         <>
             <div>
-                
+                <div>
+                    <p>Evaluate Mains Answer</p>
+                    <button>Star Evalution</button>
+                </div>
             </div>
         </>
     )
